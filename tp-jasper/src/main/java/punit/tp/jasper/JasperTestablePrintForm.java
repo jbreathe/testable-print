efaae84e7a8e313bda7c1cbaeef74484aec049a6
Artem <art.shaburov@gmail.com>
2018-06-25 23:41:56 +0700

    It works!

--- tp-jasper/src/main/java/punit/tp/jasper/JasperTestablePrintForm.java
+++ tp-jasper/src/main/java/punit/tp/jasper/JasperTestablePrintForm.java
@@ -1,34 +1,63 @@
 package punit.tp.jasper;
 
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.base.JRBaseTextField;
+import net.sf.jasperreports.engine.fill.JRTemplatePrintText;
 import punit.tp.core.PrintFormField;
 import punit.tp.core.TestablePrintForm;
-import net.sf.jasperreports.engine.JRField;
-import net.sf.jasperreports.engine.JRPrintElement;
-import net.sf.jasperreports.engine.JRPrintPage;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.design.JasperDesign;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class JasperTestablePrintForm implements TestablePrintForm<String> {
-    private JasperDesign jasperDesign;
-    private JasperPrint jasperPrint;
+    private final JasperReport jasperReport;
+    private final JasperPrint jasperPrint;
 
-    public JasperTestablePrintForm(JasperDesign jasperDesign, JasperPrint jasperPrint) {
-        this.jasperDesign = jasperDesign;
+    private Map<UUID, String> association = new HashMap<>();
+
+    public JasperTestablePrintForm(JasperReport jasperReport, JasperPrint jasperPrint) {
+        this.jasperReport = jasperReport;
         this.jasperPrint = jasperPrint;
-    }
-
-    public void parse() {
-        List<JRField> fields = jasperDesign.getFieldsList();
-        List<JRPrintPage> pages = jasperPrint.getPages();
-        for (JRPrintPage printPage : pages) {
-            List<JRPrintElement> printElements = printPage.getElements();
-        }
+        parse();
     }
 
     @Override
     public PrintFormField getField(String id) {
+        for (JRPrintPage page: jasperPrint.getPages()) {
+            for (JRPrintElement element: page.getElements()) {
+                if (association.containsKey(element.getUUID())) {
+                    String fieldName = association.get(element.getUUID());
+                    if (fieldName.equals(id)) {
+                        String fullText = ((JRTemplatePrintText) element).getFullText();
+                        return new JasperPrintFormField(fullText);
+                    }
+                }
+            }
+        }
         return null;
+    }
+
+    private void parse() {
+        for (JRChild child: jasperReport.getTitle().getChildren()) {
+            JRBaseTextField textField = (JRBaseTextField) child;
+            association.put(
+                    textField.getUUID(),
+                    ((JRBaseTextField) child).getExpression().getChunks()[0].getText()
+            );
+        }
+    }
+
+    private class JasperPrintFormField implements PrintFormField {
+        private final String value;
+
+        private JasperPrintFormField(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public String value() {
+            return value;
+        }
     }
 }
---
+++ tp-jasper/src/test/java/punit/tp/jasper/some/SomeTest.java
@@ -0,0 +1,24 @@
+package punit.tp.jasper.some;
+
+import net.sf.jasperreports.engine.*;
+import org.junit.Test;
+import punit.tp.jasper.JasperTestablePrintForm;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class SomeTest {
+    @Test
+    public void simpleTest() throws JRException {
+        InputStream is = SomeTest.class.getResourceAsStream("/Some.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(is);
+        Map<String, Object> params = new HashMap<>();
+        params.put("SongName", "I believe I can fly!");
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource());
+        JasperTestablePrintForm printForm = new JasperTestablePrintForm(jasperReport, jasperPrint);
+        assertEquals("I believe I can fly!", printForm.getField("SongName").value());
+    }
+}
